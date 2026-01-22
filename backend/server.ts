76877200245fbf5388aac717de1b503c41d19fd9
SanidhyGupta-10 <adityasgupta1857@gmail.com>
2026-01-22 18:43:39 +0530

    Fixed MongoURI and Server main file and User Model

--- backend/server.ts
+++ backend/server.ts
@@ -6,8 +6,13 @@ import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
 
-ConnectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is Running on ${PORT}`)
+ConnectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is Running on ${PORT}`)
+        });
+    })
+    .catch((err) => {
+        console.log('Failed to Connect with th server', err);
+        process.exit(1);
     });
-});
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,7 +4,10 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 export const ConnectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI as string);
+        if(!MONGODB_URI){
+            throw new Error('MONGODB_URI environment variable is not defined')
+        }
+        await mongoose.connect(MONGODB_URI);
         console.log('MongoDB Connected Successfully');
     } catch (error) {
         console.log(error);
