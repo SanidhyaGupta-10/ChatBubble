70fb1eae58a278307f39dfc030226982b20febad
SanidhyGupta-10 <adityasgupta1857@gmail.com>
2026-01-22 17:09:34 +0530

    All routes are implemented

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import authRoutes from './routes/auth.route';
+import chatRoutes from './routes/chat.route';
+import messageRoutes from './routes/message.route';
+import userRoutes from './routes/user.route';
 
 const app = express();
 const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes)
+app.use('/api/chat', chatRoutes)
+app.use('/api/messages', messageRoutes)
+app.use('/api/users', userRoutes)
 
 export default app;
