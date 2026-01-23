e7a69034427c84defa53b519dd0f19c6e12c17d1
SanidhyGupta-10 <adityasgupta1857@gmail.com>
2026-01-23 21:48:27 +0530

    Integrated Clerk Auth and auth routes and controllers

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,57 @@
+import type { Response, Request, NextFunction } from "express";
+import type { AuthRequest } from "../middlewares/auth.middleware";
+import User from "../models/User.model";
+import { clerkClient, getAuth } from "@clerk/express";
+
+export async function getMe(req: AuthRequest, res: Response, next:NextFunction) {
+    try {
+        const userId = req.userId;
+        const user = await User.findById(userId);
+
+        if (!user) {
+            res.status(404).json({
+                error: "User not found"
+            });
+            return;
+        };
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500)
+        next();
+    }
+}
+
+export async function authCallback(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { userId: clerkId } = getAuth(req);
+
+        if (!clerkId) {
+            res.status(401).json({
+                message: "Unauthorized",
+            });
+            return;
+        };
+
+        let user = await User.findOne({ clerkId });
+
+        if (!user) {
+            // get user info from clerk to save in db
+            const clerkUser = await clerkClient.users.getUser(clerkId)
+
+            user = await User.create({
+                clerkId,
+                name: clerkUser.firstName
+                    ? `${clerkUser.firstName} ${clerkUser.lastName || ""}`.trim()
+                    : clerkUser.emailAddresses[0]?.emailAddress?.split("@")[0],
+                email: clerkUser.emailAddresses[0]?.emailAddress,
+                avatar: clerkUser.imageUrl,
+            });
+        };
+
+        res.json(user)
+    } catch (error) {
+        res.status(500);
+        next();
+    };
+}
--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -2,13 +2,30 @@ import { getAuth, requireAuth } from "@clerk/express";
 import type { Request, Response, NextFunction } from "express";
 import User from "../models/User.model";
 
+export type AuthRequest = Request & {
+    userId? : string
+}
+
 export const protectedRoute = [
     requireAuth(),
-    async ( req: Request, res: Response, next: NextFunction ) => {
+    async ( req: AuthRequest, res: Response, next: NextFunction ) => {
         try {   
-                
+            const { userId: clerkId } = getAuth(req);  
+            if(!clerkId) return res.status(401).json({
+                message: "Unauthorized - Invalid Token",
+            });
+
+            const user = await User.findOne({ clerkId });
+            if(!user) return res.status(404).json({
+                 message: "User not found"
+            });
+
+            req.userId =  user._id.toString();
         } catch (error) {
-            
-        }
+            console.log(error);
+            res.status(500).json({
+                message: "Internal Server Error",
+            });
+        };
     }
-]
+];
