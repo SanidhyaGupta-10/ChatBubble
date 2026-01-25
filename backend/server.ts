03ca86c8b927941e270fa5aa2198d05e88870ca8
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-01-25 21:49:36 +0530

    Implementing Socket.io

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,18 @@
 import app from "./src/app";
-import { ConnectDB } from "./src/config/database";
 import 'dotenv/config';
-
-
+import { createServer } from 'http'
+import { ConnectDB } from "./src/config/database";
+import { initializeSocket } from "./src/utils/socket";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 ConnectDB()
     .then(() => {
-        app.listen(PORT, () => {
+        httpServer.listen(PORT, () => {
             console.log(`Server is Running on ${PORT}`)
         });
     })
