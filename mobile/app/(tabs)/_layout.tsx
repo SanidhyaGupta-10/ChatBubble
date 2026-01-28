f87c2b661e7d1378c9f93bd41ee0f545afb6e086
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-01-28 19:12:08 +0530

    Folder-Setup

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+
+const TabsLayout = () => {
+  return (
+       <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "#0D0D0F",
+            borderTopColor: "#1A1A1D",
+            borderTopWidth: 1,
+            height: 88,
+            paddingTop: 8,
+          },
+          tabBarActiveTintColor: "#F4A261",
+          tabBarInactiveTintColor: "#6B6B70",
+        }}
+       >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: "Chats",
+          tabBarIcon: ({ color, focused, size }) => (
+            <Ionicons
+              name={focused ? 'chatbubbles' : 'chatbubbles-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+         options={{
+          title: "Profile",
+          tabBarIcon: ({ color, focused, size }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-outline'}
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default TabsLayout
