f93a2f2dac616e554067266e61b315663ed8e07c
SanidhyGupta-10 <adityasgupta1857@gmail.com>
2026-02-04 19:30:04 +0530

    Home Completed and cors allowed

--- web/src/hooks/useUserSync.tsx
+++ web/src/hooks/useUserSync.tsx
@@ -6,8 +6,24 @@ import api from "../lib/axios";
 function useUserSync() {
     const {isSignedIn, getToken } = useAuth();
 
-    const {} ] 
+    const { mutate: syncUser, isPending, isSuccess } =  useMutation({
+        mutationFn: async () => {
+            const token = await getToken();
+            const res = await api.post('/api/callback', {}, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return res.data;
+        }
+    });
 
-    return {}
+    useEffect(() => {
+        if(isSignedIn && !isPending && !isSuccess) {
+            syncUser();
+        };
+    }, [isSignedIn, isPending, isSuccess, syncUser]);
+
+    return { isSynced: isSuccess, isSyncing: isPending, };
 }
-export default useUserSync
+export default useUserSync;
