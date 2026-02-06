2cb1d7125e30a3d6dbcc9a63445afb8f0d16e538
SanidhyGupta-10 <adityasgupta1857@gmail.com>
2026-02-06 18:59:51 +0530

    Error Fixed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import {  useAuth, useUser } from '@clerk/clerk-react';
+import { useAuth } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/ChatPage';
